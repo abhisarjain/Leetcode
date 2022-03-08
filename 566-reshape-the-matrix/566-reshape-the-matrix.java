7f9017cda2a04f36ccc3ce7e36ffae7dfb687a98
Abhisar Jain <84554825+abhisarjain@users.noreply.github.com>
2022-03-08 20:20:52 +0530

    Time: 1 ms (82.67%), Space: 51.4 MB (6.82%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.java
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -1,28 +1,52 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        int result[][] = new int[r][c];
-        int count=0;
-        ArrayList<Integer> temp = new ArrayList<Integer>();
-        int m = mat.length, n = mat[0].length;
-        if (m * n != r * c) return mat;
-        for(int i =0;i<mat.length;i++)
-        {
-            for(int j =0;j<mat[i].length;j++)
-        {
-            temp.add(mat[i][j]);
+//         int result[][] = new int[r][c];
+//         int count=0;
+//         ArrayList<Integer> temp = new ArrayList<Integer>();
+//         int m = mat.length, n = mat[0].length;
+//         if (m * n != r * c) return mat;
+//         for(int i =0;i<mat.length;i++)
+//         {
+//             for(int j =0;j<mat[i].length;j++)
+//         {
+//             temp.add(mat[i][j]);
                 
-        }
-        }
+//         }
+//         }
         
-        for(int i =0;i<r;i++)
-        {
-            for(int j =0;j<c;j++)
-        {
-            result[i][j] = temp.get(count);
-                count++;
+//         for(int i =0;i<r;i++)
+//         {
+//             for(int j =0;j<c;j++)
+//         {
+//             result[i][j] = temp.get(count);
+//                 count++;
                 
-        }
+//         }
+//         }
+//         return result;
+        
+        int m = mat.length;
+        int n = mat[0].length;
+        int result[][] = new int[r][c];
+        int x=0,y=0;
+        if (m * n != r * c) return mat;
+        
+        for(int i =0;i<m;i++)
+         {
+             for(int j =0;j<n;j++)
+         {
+            result[x][y] = mat[i][j];
+                 y++;
+                 if(y==c)
+                 {
+                     y=0;
+                     x++;
+                 }
+                
+         }
         }
         return result;
+        
+        
     }
 }
