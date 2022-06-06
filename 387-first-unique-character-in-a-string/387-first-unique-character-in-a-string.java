a1a270e0b66be1f878d62feb1f3921cbac2c7548
Abhisar Jain <84554825+abhisarjain@users.noreply.github.com>
2022-06-06 21:50:59 +0530

    Time: 7 ms (91.01%), Space: 48.6 MB (57.96%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,40 +1,44 @@
 class Solution {
     public int firstUniqChar(String s) {
-        LinkedHashMap<Character,Integer> hm = new LinkedHashMap<Character,Integer>();
-        // ArrayList<Integer> arr = new ArrayList<Integer>();
-        // int result[] = new int[s.length()];
+//         LinkedHashMap<Character,Integer> hm = new LinkedHashMap<Character,Integer>();
+        
        
-        for(int i=0;i<s.length();i++)
-        {
-            if(hm.containsKey(s.charAt(i)))
-            {
-                 int value = hm.get(s.charAt(i));
-                 hm.put(s.charAt(i),value+1);
-                
-                // hm.remove(s.charAt(i));
-            }
-            else
-            {
-                hm.put(s.charAt(i),1);
-            }
+//         for(int i=0;i<s.length();i++)
+//         {
+//             if(hm.containsKey(s.charAt(i)))
+//             {
+//                  int value = hm.get(s.charAt(i));
+//                  hm.put(s.charAt(i),value+1);
+         
+//             }
+//             else
+//             {
+//                 hm.put(s.charAt(i),1);
+//             }
+//         }
+       
+//         for(int i=0;i<s.length();i++)
+//         {
+//             if(hm.get(s.charAt(i))==1)
+//             {
+//                 return i;
+//             }
+//         }
+//         return -1;
+    
+        int arr[] = new int[26];
+        int n = s.length();
+        for (int i = 0; i < n; i++) {            
+            int index = s.charAt(i) - 'a';
+            arr[index]++;
         }
-        // System.out.println(hm);
-        for(int i=0;i<s.length();i++)
-        {
-            if(hm.get(s.charAt(i))==1)
-            {
+        for (int i = 0; i < n; i++) {
+            int index = s.charAt(i) - 'a';
+            if (arr[index] == 1) {
                 return i;
             }
         }
         return -1;
-       // if(hm.isEmpty() == true){return -1;}
-        // if(hm.isEmpty() == true){return -1;}
-        // else
-        // {
-        //      Map.Entry<Character,Integer> entry = hm.entrySet().iterator().next();
-        //     return entry.getValue();
-        // }
-    
       
     }
 }
