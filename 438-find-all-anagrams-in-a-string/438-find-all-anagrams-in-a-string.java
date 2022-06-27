590c49a65c51e381dcd6839316a9e94880411b1b
Abhisar Jain <84554825+abhisarjain@users.noreply.github.com>
2022-06-27 13:05:39 +0530

    Time: 105 ms (17.31%), Space: 55.4 MB (6.78%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,59 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int i = 0 ;
+        int j = 0 ;
+        int k = p.length();
+        HashMap<Character,Integer> hm1 = new HashMap<Character,Integer>();
+        List<Integer> result = new ArrayList<Integer>();
+        for(int a=0;a<k;a++)
+        {
+            if(hm1.containsKey(p.charAt(a)))
+            {
+                hm1.put(p.charAt(a),hm1.get(p.charAt(a))+1);
+            }
+            else
+            {
+                hm1.put(p.charAt(a),1);
+            }
+        }
+        
+        
+        HashMap<Character,Integer> hm2 = new HashMap<Character,Integer>();
+        while(j<s.length())
+        {
+            if(hm2.containsKey(s.charAt(j)))
+            {
+                hm2.put(s.charAt(j),hm2.get(s.charAt(j))+1);
+            }
+            else
+            {
+                hm2.put(s.charAt(j),1);
+            }
+            
+            if((j-i+1)<k)
+            {
+                j++;
+            }
+            
+            else if((j-i+1)==k)
+            {
+                if(hm2.equals(hm1))
+                {
+                    result.add(i);
+                }
+                if(hm2.get(s.charAt(i))>1)
+                {
+                     hm2.put(s.charAt(i),hm2.get(s.charAt(i))-1);
+                }
+                else
+                {
+                    hm2.remove(s.charAt(i));
+                }
+                i++;
+                j++;
+               
+            }
+        }
+        return result; 
+    }
+}
