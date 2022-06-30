8b3147eb3d4b80fe9cd6ef19113a60a344929e1b
Abhisar Jain <84554825+abhisarjain@users.noreply.github.com>
2022-07-01 01:15:07 +0530

    Time: 35 ms (62.67%), Space: 72.8 MB (77.78%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.java
+++ 525-contiguous-array/525-contiguous-array.java
@@ -1,32 +1,49 @@
-class Solution {
+// class Solution {
+//     public int findMaxLength(int[] nums) {
+//         int sum=0;
+//         int result=0;
+//         HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(nums[i]==0)
+//             {
+//                 sum = sum-1;;
+//             }
+//             else
+//             {
+//                 sum = sum+1;;
+//             }
+//             if(sum==0)
+//             {
+//                 result = Math.max(result,i+1);
+//             }
+//             else if(hm.containsKey(sum))
+//             {
+//                 result = Math.max(result,i-hm.get(sum));
+//             }
+//             else
+//             {
+//                 hm.put(sum,i);
+//             }
+//         }
+        
+//         return result;
+//     }
+// }
+public class Solution {
+
     public int findMaxLength(int[] nums) {
-        int sum=0;
-        int result=0;
-        HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==0)
-            {
-                sum = sum-1;;
-            }
-            else
-            {
-                sum = sum+1;;
-            }
-            if(sum==0)
-            {
-                result = Math.max(result,i+1);
-            }
-            else if(hm.containsKey(sum))
-            {
-                result = Math.max(result,i-hm.get(sum));
-            }
-            else
-            {
-                hm.put(sum,i);
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int maxlen = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = count + (nums[i] == 1 ? 1 : -1);
+            if (map.containsKey(count)) {
+                maxlen = Math.max(maxlen, i - map.get(count));
+            } else {
+                map.put(count, i);
             }
         }
-        
-        return result;
+        return maxlen;
     }
 }
