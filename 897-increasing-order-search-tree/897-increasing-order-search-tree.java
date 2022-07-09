d6910fb87dbacf490bfdb2bcfe0b7d425cf18752
Abhisar Jain <84554825+abhisarjain@users.noreply.github.com>
2022-07-10 01:33:13 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (67.71%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.java
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.java
@@ -14,34 +14,48 @@
  * }
  */
 class Solution {
+    TreeNode current = new TreeNode(0);
     public TreeNode increasingBST(TreeNode root) {
-        List<Integer> r = new ArrayList<Integer>();
-        if(root==null){return null;}
-        in(r,root);
+//         List<Integer> r = new ArrayList<Integer>();
+//         if(root==null){return null;}
+//         in(r,root);
         
-        TreeNode head = new TreeNode(0);
-        TreeNode temp = head;
-        int i=0;
-        // System.out.print(r);
-        while(i<r.size())
-        {
+//         TreeNode head = new TreeNode(0);
+//         TreeNode temp = head;
+//         int i=0;
+//         // System.out.print(r);
+//         while(i<r.size())
+//         {
             
-            TreeNode t = new TreeNode(r.get(i));
-            temp.right = t;
-            temp = temp.right;
-            i++;
-        }
+//             TreeNode t = new TreeNode(r.get(i));
+//             temp.right = t;
+//             temp = temp.right;
+//             i++;
+//         }
+        TreeNode ans = current;
+        helper(root);
         
-        return head.right;
+        
+        return ans.right;
     }
     
-   public void in(List<Integer> r,TreeNode root)
-    {
-     if(root==null){return;}
-        in(r,root.left);
-        r.add(root.val);
-        in(r,root.right);
+//    public void in(List<Integer> r,TreeNode root)
+//     {
+//      if(root==null){return;}
+//         in(r,root.left);
+//         r.add(root.val);
+//         in(r,root.right);
         
+//     }
+    
+    public void helper(TreeNode root)
+    {
+        if(root==null){return;}
+        helper(root.left);
+        root.left = null;
+        current.right = root;
+        current = root;
+        helper(root.right);
     }
     
     
