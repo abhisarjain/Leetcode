4eca96a6ef8e36cf5192711fba19f6eb736ebe88
Abhisar Jain <84554825+abhisarjain@users.noreply.github.com>
2022-07-15 11:21:42 +0530

    Time: 62 ms (57.00%), Space: 75 MB (54.74%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,41 +1,69 @@
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         if(nums.length==1) return 1;
+        
+//         HashSet<Integer> hs = new HashSet<>();
+//         int result =0;
+        
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             hs.add(nums[i]);
+//         }
+        
+        
+//         for(int i = 0;i<nums.length;i++)
+//         {
+//             if(hs.contains(nums[i]-1))
+//             {
+                
+//             }
+            
+//             else if(!hs.contains(nums[i]-1))
+//             {
+//                 // int x =0;
+//                 int j = 1;
+//                 while(hs.contains(nums[i]+j))
+//                 {
+//                     j++;
+//                     // if(hs.contains(nums[i]+j))
+//                     // {
+//                     //      x++;
+//                     //     j++;
+//                     // }
+//                     // else break;
+//                 }
+//                 result = Math.max(j,result);
+//             }
+//         }
+        
+//         return result;
+//     }
+// }
+
+
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length==1) return 1;
-        
-        HashSet<Integer> hs = new HashSet<>();
-        int result =0;
-        
-        for(int i=0;i<nums.length;i++)
-        {
-            hs.add(nums[i]);
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
         }
-        
-        
-        for(int i = 0;i<nums.length;i++)
-        {
-            if(hs.contains(nums[i]-1))
-            {
-                
-            }
-            
-            else if(!hs.contains(nums[i]-1))
-            {
-                // int x =0;
-                int j = 1;
-                while(hs.contains(nums[i]+j))
-                {
-                    j++;
-                    // if(hs.contains(nums[i]+j))
-                    // {
-                    //      x++;
-                    //     j++;
-                    // }
-                    // else break;
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num-1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum+1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
                 }
-                result = Math.max(j,result);
+
+                longestStreak = Math.max(longestStreak, currentStreak);
             }
         }
-        
-        return result;
+
+        return longestStreak;
     }
 }
